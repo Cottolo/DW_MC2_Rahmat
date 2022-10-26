f821260002d4885bc02d955e3fc13d6b405f2879
unknown <rahmat9654@gmail.com>
2022-10-26 18:30:29 +0700

    update

--- Handlers/HandlePowerUP.go
+++ Handlers/HandlePowerUP.go
@@ -6,9 +6,9 @@ func PowerUp(Profile models.Profile, nilai int) models.Profile {
 
 	var PowerUp models.Profile
 	PowerUp.Name = Profile.Name
-	PowerUp.Health = Profile.Health * nilai
-	PowerUp.Power = Profile.Power * nilai
-	PowerUp.Exp = Profile.Exp * nilai
+	PowerUp.Health = Profile.Health + (Profile.Health * nilai)
+	PowerUp.Power = Profile.Power + (Profile.Power * nilai)
+	PowerUp.Exp = Profile.Exp + (Profile.Exp * nilai)
 
 	return PowerUp
 }
